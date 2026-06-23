9e7a790ec1c485f4a5ac584cb0fa64d474f81250
Fuhan Yang <ab32@cdc.gov>
2026-06-23 16:43:16 -0400

    add conditionals

--- scripts/get_forecast_dates.py
+++ scripts/get_forecast_dates.py
@@ -1,4 +1,5 @@
 import argparse
+from datetime import date
 
 import polars as pl
 import yaml
@@ -18,4 +19,17 @@ if __name__ == "__main__":
         eager=True,
     )
 
-    print(*forecast_dates, sep=" ")
+    if forecast_dates[0] < date(
+        config["season"]["start_year"],
+        config["season"]["start_month"],
+        config["season"]["start_day"],
+    ):
+        print("forecast date out of data range. ")
+    elif forecast_dates[-1] > date(
+        config["season"]["end_year"],
+        config["season"]["end_month"],
+        config["season"]["end_day"],
+    ):
+        print("forecast date out of data range. ")
+    else:
+        print(*forecast_dates, sep=" ")
