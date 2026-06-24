21843346cce5ec6c03b4d61921d8b0e373414680
Scott Olesen <ulp7@cdc.gov>
2026-06-24 11:02:48 -0400

    Check date boundaries

--- scripts/get_forecast_dates.py
+++ scripts/get_forecast_dates.py
@@ -12,6 +12,21 @@ if __name__ == "__main__":
     with open(args.config) as f:
         config = yaml.safe_load(f)
 
+    first_date = date(
+        config["season"]["start_year"],
+        config["season"]["start_month"],
+        config["season"]["start_day"],
+    )
+
+    last_date = date(
+        config["season"]["end_year"],
+        config["season"]["end_month"],
+        config["season"]["end_day"],
+    )
+
+    assert config["forecast_dates"]["start"] >= first_date
+    assert config["forecast_dates"]["end"] <= last_date
+
     forecast_dates = pl.date_range(
         config["forecast_dates"]["start"],
         config["forecast_dates"]["end"],
@@ -19,17 +34,4 @@ if __name__ == "__main__":
         eager=True,
     )
 
-    if forecast_dates[0] < date(
-        config["season"]["start_year"],
-        config["season"]["start_month"],
-        config["season"]["start_day"],
-    ):
-        print("forecast date out of data range. ")
-    elif forecast_dates[-1] > date(
-        config["season"]["end_year"],
-        config["season"]["end_month"],
-        config["season"]["end_day"],
-    ):
-        print("forecast date out of data range. ")
-    else:
-        print(*forecast_dates, sep=" ")
+    print(*forecast_dates, sep=" ")
